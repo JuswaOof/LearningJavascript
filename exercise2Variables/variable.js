5f1e333b25173a0ba5d9dab35264d1be1ec8b2fe
Joshua Salcedo <71166003+JuswaOof@users.noreply.github.com>
2023-09-30 12:02:47 +0800

    Update variable.js
    added code for exercise2Variable

--- exercise2Variables/variable.js
+++ exercise2Variables/variable.js
@@ -1,1 +1,13 @@
+// variable = container for storing data
 
+let firstname = 'Josh' //string
+let age = 25 //number or int
+let student = false //boolean
+
+console.log(firstname)
+console.log(age)
+console.log(student)
+
+document.getElementById('p1').innerHTML = 'Hello ' + firstname
+document.getElementById('p2').innerHTML = 'You are ' + age + ' years old'
+document.getElementById('p3').innerHTML = 'Enrolled: ' + student
