01003e7fa471406ddc437a44595f6d0940d4416e
Joshua Salcedo <71166003+JuswaOof@users.noreply.github.com>
2023-11-06 14:31:33 +0800

    Update arithmeticExpression.js

--- exercise3ArithmeticExpression/arithmeticExpression.js
+++ exercise3ArithmeticExpression/arithmeticExpression.js
@@ -1,1 +1,9 @@
+let students = 20;
 
+// students = students + 1 //addition
+// students = students - 1 //subtraction
+// students = students * 2 //multiplication
+// students = students / 2 //division
+// students = students % 2 //modulo (will get the remainder, commonly used for getting odd or even when u modulo by 2)
+
+console.log(students);
