93bba744468c4c897519d60a68391aba4e412ac5
Joshua Salcedo <71166003+JuswaOof@users.noreply.github.com>
2023-11-10 14:40:18 +0800

    Update typeConversion.js

--- exercise5TypeConversion/typeConversion.js
+++ exercise5TypeConversion/typeConversion.js
@@ -1,1 +1,21 @@
+// let age = window.prompt('How old are you?')
 
+// console.log(typeof age)
+// age = Number(age)
+// age += 1
+// console.log(typeof age)
+// console.log('Total number is: ' + age)
+
+let x
+let y
+let z
+
+x = Number('3.14')
+// if the input can not be converted to a number it will output NaN(not a number)
+y = String(3.14)
+z = Boolean('asdf')
+// if it has content it will output true if it didnt have it will output false
+
+console.log(x, typeof x)
+console.log(y, typeof y)
+console.log(z, typeof z)
