abe6d4d26e332eef46cf95774a43701351336bc4
Joshua Salcedo <71166003+JuswaOof@users.noreply.github.com>
2023-11-11 09:15:08 +0800

    Update const.js

--- exercise6Const/const.js
+++ exercise6Const/const.js
@@ -1,1 +1,14 @@
+// const = variable that can not be changed
 
+const PI = 3.14159
+let radius
+let circumference
+
+radius = window.prompt('Please enter radius')
+radius = Number(radius)
+
+// PI = 6969
+
+circumference = 2 * PI * radius
+
+console.log('The circumference is: ' + circumference)
