11db8fc6f9649db2311ffd66e416cfb7f405337f
Joshua Salcedo <71166003+JuswaOof@users.noreply.github.com>
2023-11-12 18:43:41 +0800

    Create math.js

---
+++ exercise7Math/math.js
@@ -0,0 +1,27 @@
+let x = 3.14
+let y = 5
+let z = 15
+let minimum
+let maximum
+
+// x = Math.round(x)
+// round off
+// x = Math.floor(x)
+// round down
+// x = Math.ceil(x)
+// round up
+// x = Math.pow(x, 5)
+// exponent
+// x = Math.sqrt(x)
+// square root
+// x = Math.abs(x)
+// absolute
+
+minimum = Math.min(x, y, z)
+maximum = Math.max(x, y, z)
+
+console.log(minimum)
+console.log(maximum)
+
+x = Math.PI
+console.log(x)
